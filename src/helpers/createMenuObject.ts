6df10c7bd3774f9f401dc1c9ec1be1dbd22c1549
williamlunelli <williamlunelli07@gmail.com>
2025-02-08 09:18:15 -0300

    Activating Menu animation by helpers

---
+++ src/helpers/createMenuObject.ts
@@ -0,0 +1,16 @@
+type MenuOption = '' | 'all' | 'dogs' | 'cats' | 'fishes'
+
+export const createMenuObject = (activeMenu: MenuOption) => {
+    let returnObject = {
+        all: false,
+        dogs: false,
+        cats: false,
+        fishes: false
+    }
+
+    if(activeMenu !== ''){
+        returnObject[activeMenu] = true
+    }
+
+    return returnObject
+}
