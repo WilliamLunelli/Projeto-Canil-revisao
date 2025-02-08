5b97e65013b08554e331a5c5be4685c09430f2e2
williamlunelli <williamlunelli07@gmail.com>
2025-02-08 10:11:14 -0300

    Creating the search bar functions and page 404

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,22 @@
 import {Request, Response} from 'express';
+import { Pet } from '../Models/Pet';
+import { createMenuObject } from '../Helpers/createMenuObject';
 
 export const search = (req: Request, res: Response) => {
+
+    let query: string = req.query.q as string;
+
+    if(!query){
+        res.redirect('/');
+        return;
+    }
+
+    let list = Pet.getFromName(query);
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    })
 
 }
