feeb00ac28239b82a306c7b5d1a1313ff5868b2b
williamlunelli <williamlunelli07@gmail.com>
2025-02-08 09:04:46 -0300

    Modifying data on each page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,8 +5,8 @@ import * as searchController from '../controllers/searchController'
 const router = Router();
 
 router.get('/', pageController.home);
-router.get('/', pageController.dogs);
-router.get('/', pageController.cats);
-router.get('/', pageController.fishes);
+router.get('/dogs', pageController.dogs);
+router.get('/cats', pageController.cats);
+router.get('/fishes', pageController.fishes);
 router.get('/search', searchController.search)
 export default router;
